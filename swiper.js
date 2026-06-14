e94387df108fcfc826079d7712d31db5eeb3caec
Farzam Nasiripour <farzam.nasiripour0@gmail.com>
2026-06-14 23:51:53 +0330

    feat(inventory-Card):add swiper-2 js

--- swiper.js
+++ swiper.js
@@ -1,5 +1,23 @@
-var swiper = new Swiper(".mySwiper", {
+var swiper_1 = new Swiper(".mySwiper_1", {
   effect: "cards",
   grabCursor: true,
   loop: true,
 });
+// ///////////////////////////////////////////////// //
+var swiper_2 = new Swiper(".mySwiper_2", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+// ///////////////////////////////////////////////// //
