7ed0643e014b8472c6d7a0db81a7a196f4a8a5fb
Farzam Nasiripour <farzam.nasiripour0@gmail.com>
2026-06-22 18:28:12 +0330

    feat(js):mk main.js for Hamburger Menu

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const btn_Animation = document.getElementById("menuBtn");
+
+btn_Animation.addEventListener("click", () => {
+  btn_Animation.classList.toggle("active");
+
+  document.querySelector(".line1").classList.toggle("top-3");
+  document.querySelector(".line1").classList.toggle("top-1/2");
+  document.querySelector(".line1").classList.toggle("-translate-y-1/2");
+  document.querySelector(".line1").classList.toggle("rotate-45");
+
+  document.querySelector(".line2").classList.toggle("opacity-0");
+  document.querySelector(".line2").classList.toggle("scale-x-0");
+
+  document.querySelector(".line3").classList.toggle("bottom-3");
+  document.querySelector(".line3").classList.toggle("top-1/2");
+  document.querySelector(".line3").classList.toggle("-translate-y-1/2");
+  document.querySelector(".line3").classList.toggle("-rotate-45");
+});
+// //////////////////////////////////////////////////////////////////// //
+
+
