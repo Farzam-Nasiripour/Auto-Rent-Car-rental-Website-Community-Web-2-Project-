007f07fa06db0fc013eab1fbce6c0fc1f4ef5f0f
Farzam Nasiripour <farzam.nasiripour0@gmail.com>
2026-07-05 01:08:08 +0330

    feat(header):header pc finished Add link for header-Mobile-Tablet responsive fix : 6-swiper new 100% 7-tutorial 100% 8-question 9-customer 10-articles 11-footer ----------------------------------------- --> index.html is 90% responsive <-- -----------------------------------------

--- swiper.js
+++ swiper.js
@@ -1,9 +1,10 @@
+// //////////////////// swiper-1 /////////////////// //
 var swiper_1 = new Swiper(".mySwiper_1", {
   effect: "cards",
   grabCursor: true,
   loop: true,
 });
-// ///////////////////////////////////////////////// //
+// //////////////////// swiper-2 /////////////////// //
 var swiper_2 = new Swiper(".mySwiper_2", {
   effect: "coverflow",
   grabCursor: true,
@@ -18,6 +19,23 @@ var swiper_2 = new Swiper(".mySwiper_2", {
   },
   pagination: {
     el: ".swiper-pagination",
+  },
+});
+// //////////////////// swiper-3 /////////////////// //
+var swiper_3 = new Swiper(".mySwiper_3", {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
   },
 });
 // ///////////////////////////////////////////////// //
