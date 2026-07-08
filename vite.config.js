195069bfbd847b092477c46d3648c2f8bce3ea02
Farzam Nasiripour <farzam.nasiripour0@gmail.com>
2026-07-09 00:12:49 +0330

    test

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: "/Auto-Rent-Car-rental-Website-Community-Web-2-Project-/",
 });
